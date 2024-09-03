4b90ce8edd874dda9c4b6ef39912fffa984a2e00
Ogua ahmed <ogua.ahmed18@gmail.com>
2024-09-03 02:18:30 +0000

    updated item'

--- Restaurant-admin/database/factories/MenuitemFactory.php
+++ Restaurant-admin/database/factories/MenuitemFactory.php
@@ -6,20 +6,46 @@ use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menuitem>
- */
+* @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menuitem>
+*/
 class MenuitemFactory extends Factory
 {
     /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    * Define the model's default state.
+    *
+    * @return array<string, mixed>
+    */
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word(),
-            'photo' => $this->faker->imageUrl(),
+            'name' => $this->faker->randomElement([
+                "Burger",
+                "Pizza",
+                "Pasta",
+                "Salad",
+                "Steak",
+                "Sushi",
+                "Tacos",
+                "Ice Cream",
+                "Sandwich",
+                "Soup",
+                "Fries",
+                "Tea",   
+                "Salad", 
+            ]),
+            'photo' => $this->faker->randomElement([
+                "https://plus.unsplash.com/premium_photo-1663036447682-8f0d918adbed?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "https://images.unsplash.com/photo-1544025162-d76694265947?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+                "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D",
+                "https://images.unsplash.com/photo-1676300185292-e23bb3db50fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE5fHx8ZW58MHx8fHx8",
+                "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YnVyZ2VyfGVufDB8fDB8fHww",
+                "https://images.unsplash.com/photo-1512152272829-e3139592d56f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D",
+                "https://images.unsplash.com/photo-1504937551116-cb8097e6f02a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2FuZHdpY2h8ZW58MHx8MHx8fDA%3D",
+                "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGl6emF8ZW58MHx8MHx8fDA%3D",
+                "https://images.unsplash.com/photo-1579751626657-72bc17010498?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fHww",
+                "https://plus.unsplash.com/premium_photo-1673545518947-ddf3240090b1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29mZmVlfGVufDB8fDB8fHww",
+                "https://images.unsplash.com/photo-1546793665-c74683f339c1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNhbGFkfGVufDB8fDB8fHww",
+            ]),
             'description' => $this->faker->text(),
             'price' => $this->faker->numberBetween(10, 100),
             'category_id' => Category::factory(),
